98cc14d585900effa9cc06a632aae4950201258e
Wesley Rodrigues <74609771+WesleyRodrigues55@users.noreply.github.com>
2021-04-03 14:28:24 -0300

    Em andamento
    Problemas na sessão de login, falha em que permite que navegue pela url passando pelo login

--- usuarioERRO.php
+++ usuarioERRO.php
@@ -5,11 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sem permissão</title>
+    <script>
+        function alerta() {
+            alert('Sem permissão!');
+            window.location.href = 'usuarioLOGIN.php';
+        }
+    </script>
 </head>
-<body>
-
-<h3>Você não tem permissão para acessar esta página</h3>
-<a href="usuarioLOGIN.php">Clique aqui para efetuar o login</a>
+<body onload="alerta()">
     
 </body>
 </html>
